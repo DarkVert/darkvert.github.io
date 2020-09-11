e6dacebd8dd276bf0d73a4b882c167282c8c5891
DarkVert <69045538+DarkVert@users.noreply.github.com>
2020-09-11 12:29:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     nav: true,
+    center: false,
+    autoWidth: false,
     navText: ["", ""],
     responsive: {
       0: {
-        items: 1,
+        items: 2,
+        center: false,
       },
       480: {
         items: 2,
+        center: false,
       },
       980: {
         items: 3,
